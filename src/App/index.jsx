de8692199dc92f877cebe975f75a25c68629436d
Edwin Obando <edwin.obando@vtex.com.br>
2022-03-02 19:42:57 -0500

    update base template, use create react app insted

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,94 +1,94 @@
 /* eslint-disable react/jsx-indent */
-import React from 'react';
-import './styles.scss';
+import React from "react";
+import "./styles.scss";
 
 // Material Desing
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import CssBaseLine from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
+import CssBaseLine from "@material-ui/core/CssBaseline";
 
 // Components
-import HomePage from '../Pages/Home Page';
+import HomePage from "../Pages/Home Page";
 
 const themeJSON = {
     palette: {
         common: {
-            black: '#000',
-            white: '#fff',
+            black: "#000",
+            white: "#fff",
         },
         background: {
-            paper: '#fff',
-            default: '#fafafa',
+            paper: "#fff",
+            default: "#fafafa",
         },
         primary: {
-            light: '#2B2B2B',
-            main: '#313131',
-            dark: '#000',
-            contrastText: '#fff',
+            light: "#2B2B2B",
+            main: "#313131",
+            dark: "#000",
+            contrastText: "#fff",
         },
         secondary: {
-            light: '#f37f33',
-            main: '#f06000',
-            dark: '#a84300',
-            contrastText: '#fff',
+            light: "#f37f33",
+            main: "#f06000",
+            dark: "#a84300",
+            contrastText: "#fff",
         },
         error: {
-            light: 'rgba(255, 91, 106, 1)',
-            main: 'rgba(255, 0, 63, 1)',
-            dark: 'rgba(195, 0, 25, 1)',
-            contrastText: '#fff',
+            light: "rgba(255, 91, 106, 1)",
+            main: "rgba(255, 0, 63, 1)",
+            dark: "rgba(195, 0, 25, 1)",
+            contrastText: "#fff",
         },
         text: {
-            primary: 'rgb(255, 255, 255)',
-            secondary: 'rgba(85, 85, 85, 0.8)',
-            disabled: 'rgba(0, 0, 0, 0.38)',
-            hint: 'rgba(0, 0, 0, 0.38)',
+            primary: "rgb(255, 255, 255)",
+            secondary: "rgba(85, 85, 85, 0.8)",
+            disabled: "rgba(0, 0, 0, 0.38)",
+            hint: "rgba(0, 0, 0, 0.38)",
         },
     },
     overrides: {
         MuiCssBaseline: {
-            '@global': {
-                '*': {
-                    'scrollbar-width': 'thin',
+            "@global": {
+                "*": {
+                    "scrollbar-width": "thin",
                 },
-                '*::webkit-scrollbar': {
-                    width: '0.2em',
-                    height: '0.2em',
+                "*::webkit-scrollbar": {
+                    width: "0.2em",
+                    height: "0.2em",
                 },
             },
         },
     },
 };
 
-const EOTheme = createMuiTheme(themeJSON);
+const EOTheme = createTheme(themeJSON);
 
 EOTheme.typography.h1 = {
-    fontSize: '2.4rem',
-    fontWeight: 'bold',
+    fontSize: "3rem",
+    fontWeight: "bold",
 };
 
 EOTheme.typography.h2 = {
-    fontSize: '2rem',
-    fontWeight: 'bold',
+    fontSize: "2.2rem",
+    fontWeight: "bold",
 };
 
 EOTheme.typography.h3 = {
-    fontSize: '1.6rem',
-    fontWeight: 'bold',
+    fontSize: "1.6rem",
+    fontWeight: "bold",
 };
 
 EOTheme.typography.h4 = {
-    fontSize: '1.2rem',
-    fontWeight: 'bold',
+    fontSize: "1.2rem",
+    fontWeight: "bold",
 };
 
 EOTheme.typography.h5 = {
-    fontSize: '1rem',
-    fontWeight: 'bold',
+    fontSize: "1rem",
+    fontWeight: "bold",
 };
 
 EOTheme.typography.h6 = {
-    fontSize: '0.85rem',
-    fontWeight: 'bold',
+    fontSize: "0.85rem",
+    fontWeight: "bold",
 };
 
 const App = () => (
