5172ac7298d5968a89f79b375b9703aa621d91f9
Edwin Obando <edwin.obando@vtex.com.br>
2022-08-24 23:19:29 -0500

    refactor: :sparkles: refactor basic template

---
+++ components/layout.tsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Layout = ({ children }: React.PropsWithChildren) => {
+    return (
+        <>
+            <Header />
+            <main>{children}</main>
+            <Footer />
+        </>
+    );
+};
+
+export default Layout;
