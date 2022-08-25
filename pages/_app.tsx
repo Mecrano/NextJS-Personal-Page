abd5a624b2eb0d88d9e63ca0da7e35379eb922a6
Edwin Obando <edwin.obando@vtex.com.br>
2022-08-24 23:19:59 -0500

    style: :lipstick: formatting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app'
 
-import Layout from "../components/layout";
-import "../styles/globals.css";
+import Layout from '../components/layout'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
-export default MyApp;
+export default MyApp
