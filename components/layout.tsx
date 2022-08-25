647004801a54959bbbb09d88db96f1e0df616b25
Edwin Obando <edwin.obando@vtex.com.br>
2022-08-25 00:52:42 -0500

    feat: :lipstick: add fonts

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head'
+
 import Footer from './Footer'
 import Header from './Header'
 
 function Layout({ children }: React.PropsWithChildren) {
   return (
     <>
+      <Head>
+        <title>Edwin Obando - Portfolio</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <main>{children}</main>
       <Footer />
