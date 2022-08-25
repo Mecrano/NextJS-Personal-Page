e8e556b4b5c37e26bfbf70edf06f4dd9bf43a423
Edwin Obando <edwin.obando@vtex.com.br>
2022-08-25 10:22:22 -0500

    feat: :chart_with_upwards_trend: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import { NextIntlProvider } from 'next-intl'
 
 import Layout from '../components/layout'
+import GA from '../components/GA'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -20,6 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       now={new Date(pageProps.now)}
       timeZone="America/Bogota"
     >
+      <GA />
       <Layout>
         <Component {...pageProps} />
       </Layout>
