739e0024566e5eb1eab41afdb4731065bd5f2d40
Edwin Obando <edwin.alex1995@gmail.com>
2022-08-26 15:34:42 -0500

    feat: :fire: create theme hook

---
+++ hooks/useTheme/index.ts
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+
+const useTheme = () => {
+  const [theme, setLocalTheme] = useState<string | null>(null)
+
+  const setTheme = (newTheme: string) => {
+    const html = document.querySelector(':root')
+
+    // eslint-disable-next-line vtex/prefer-early-return
+    if (html) {
+      html.className = newTheme
+      localStorage.setItem('theme', newTheme)
+      setLocalTheme(newTheme)
+    }
+  }
+
+  useEffect(() => {
+    const storageTheme = window?.localStorage?.getItem('theme')
+
+    if (storageTheme) {
+      setLocalTheme(storageTheme)
+      setTheme(storageTheme)
+    } else if (window?.matchMedia) {
+      const themeHandler = window.matchMedia('(prefers-color-scheme: dark)')
+
+      setLocalTheme(themeHandler.matches ? 'dark' : 'light')
+
+      themeHandler.addEventListener('change', (event) => {
+        if (!window?.localStorage?.getItem('theme')) {
+          setLocalTheme(event.matches ? 'dark' : 'light')
+        }
+      })
+    } else {
+      setLocalTheme('light')
+    }
+  }, [])
+
+  return { theme, setTheme }
+}
+
+export default useTheme
