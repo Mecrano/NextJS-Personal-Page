ab74d21c1288a743f90f314cfe1e26f51777d2b9
Edwin Obando <edwin.alex1995@gmail.com>
2022-08-26 23:20:09 -0500

    feat: :lipstick: create footer and make Navigation components global

--- components/global/Footer/index.tsx
+++ components/global/Footer/index.tsx
@@ -1,5 +1,30 @@
-function Footer() {
-  return <footer />
+import { UilGithubAlt, UilLinkedinAlt } from '@iconscout/react-unicons'
+import { useTranslations } from 'next-intl'
+
+import Navigation from '../Navigation'
+import styles from './styles.module.css'
+
+const Footer = () => {
+  const t = useTranslations('Footer')
+
+  const links = [
+    {
+      href: 'https://github.com/Mecrano',
+      label: <UilGithubAlt />,
+    },
+    {
+      href: 'https://www.linkedin.com/in/eobando/',
+      label: <UilLinkedinAlt />,
+    },
+  ]
+
+  return (
+    <footer className={`flex-center flex-col ${styles.mainFooter}`}>
+      <p className="m-1">{t('sentence')}</p>
+      <p className="m-1 mb-6">{t('copyright')}</p>
+      <Navigation links={links} />
+    </footer>
+  )
 }
 
 export default Footer
--- components/global/Header/index.tsx
+++ components/global/Header/index.tsx
@@ -1,14 +1,43 @@
+import { UilGithubAlt, UilLinkedinAlt } from '@iconscout/react-unicons'
+import { useTranslations } from 'next-intl'
+
 import Back from './Back'
-import Navigation from './Navigation'
 import ThemeSwitcher from './ThemeSwitcher'
+import Navigation from '../Navigation'
+import styles from './styles.module.css'
 
 const Header = () => {
+  const t = useTranslations('Header')
+
+  const links = [
+    {
+      href: '#about',
+      label: t('links.about'),
+    },
+    {
+      href: '#projects',
+      label: t('links.projects'),
+    },
+    {
+      href: '#blog',
+      label: t('links.blog'),
+    },
+    {
+      href: 'https://www.linkedin.com/in/eobando/',
+      label: <UilLinkedinAlt />,
+    },
+    {
+      href: 'https://github.com/Mecrano',
+      label: <UilGithubAlt />,
+    },
+  ]
+
   return (
-    <header className="flex-center w-full my-5">
+    <header className={`${styles.mainHeader} flex-center w-full my-5 sticky top-0`}>
       <div className="flex justify-between w-full max-w-6xl p-3">
         <Back />
         <div className="flex justify-end items-center gap-x-10">
-          <Navigation />
+          <Navigation links={links} />
           <ThemeSwitcher />
         </div>
       </div>
