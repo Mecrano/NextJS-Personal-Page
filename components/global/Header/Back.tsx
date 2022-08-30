0d6629ec15257c7b99f9e05e90c472221d2a4e4e
Edwin Obando <edwin.alex1995@gmail.com>
2022-08-29 19:51:14 -0500

    feat: :heavy_minus_sign: remove back component

--- components/global/Header/Back.tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router'
-
-const Back = () => {
-  const router = useRouter()
-
-  return router.pathname !== '/' ? <div>Back</div> : <div></div>
-}
-
-export default Back
