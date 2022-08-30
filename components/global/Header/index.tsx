efc3466549d7bf236db1261cf3731036828881c7
Edwin Obando <edwin.alex1995@gmail.com>
2022-08-29 19:56:07 -0500

    feat: :art: improve UI of header

--- components/global/Header/index.tsx
+++ components/global/Header/index.tsx
@@ -1,43 +1,49 @@
 import { UilGithubAlt, UilLinkedinAlt } from '@iconscout/react-unicons'
 import { useTranslations } from 'next-intl'
 
-import Back from './Back'
 import ThemeSwitcher from './ThemeSwitcher'
-import Navigation from '../Navigation'
+import LanguageSwitcher from './LanguageSwitcher'
+import NavigationMenu from '../NavigationMenu'
 import styles from './styles.module.css'
 
 const Header = () => {
   const t = useTranslations('Header')
 
-  const links = [
+  const navigationLinks = [
     {
       href: '#about',
+      type: 'text',
       label: t('links.about'),
     },
     {
       href: '#projects',
+      type: 'text',
       label: t('links.projects'),
     },
     {
       href: '#blog',
+      type: 'text',
       label: t('links.blog'),
     },
     {
       href: 'https://www.linkedin.com/in/eobando/',
+      type: 'icon',
       label: <UilLinkedinAlt />,
     },
     {
       href: 'https://github.com/Mecrano',
+      type: 'icon',
       label: <UilGithubAlt />,
     },
   ]
 
   return (
-    <header className={`${styles.mainHeader} flex-center w-full mt-5 sticky top-0`}>
+    <header className={`${styles.mainHeader} flex-center w-full sm:mt-5 sticky top-0 z-50`}>
       <div className="flex justify-between w-full max-w-7xl p-3">
-        <Back />
-        <div className="flex justify-end items-center gap-x-10">
-          <Navigation links={links} />
+        <NavigationMenu links={navigationLinks} />
+
+        <div className="flex justify-end items-center gap-x-2">
+          <LanguageSwitcher />
           <ThemeSwitcher />
         </div>
       </div>
