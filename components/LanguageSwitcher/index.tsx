951b27f927bc11ed5d3b39c92eb5cae921ace0f1
Edwin Obando <edwin.alex1995@gmail.com>
2022-08-30 13:13:29 -0500

    refactor: :technologist: change Language Switcher component to Smart and Dumb component concept

--- components/LanguageSwitcher/index.tsx
+++ components/LanguageSwitcher/index.tsx
@@ -1,34 +1,11 @@
-import type { ChangeEvent } from 'react'
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
-import { useTranslations } from 'next-intl'
-
 import DropDown from 'components/DropDown'
 
-const LanguageSwitcher = () => {
-  const { locale, push, pathname, locales } = useRouter()
-  const [language, setLanguage] = useState(locale ?? 'en')
-  const t = useTranslations('Header.languageOptions')
+import useLanguageSwitcher from './useLanguageSwitcher'
 
-  const languageOptions =
-    locales?.map((lang: string | any) => ({
-      value: lang,
-      label: t(lang),
-    })) ?? []
-
-  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    setLanguage(event.target.value)
-  }
-
-  useEffect(() => {
-    if (language !== locale) {
-      push(pathname, '', {
-        locale: language,
-      })
-    }
-  }, [language, locale, pathname, push])
+const LanguageSwitcherContainer = () => {
+  const { language, handleChange, languageOptions } = useLanguageSwitcher()
 
   return <DropDown value={language} onChange={handleChange} options={languageOptions} />
 }
 
-export default LanguageSwitcher
+export default LanguageSwitcherContainer
