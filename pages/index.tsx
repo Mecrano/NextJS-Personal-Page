0b32538de2faeaa707c8a5e51abc34060eeb015e
Edwin Obando <edwin.alex1995@gmail.com>
2023-07-10 18:23:21 -0500

    style: fix identation

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,14 +31,22 @@ const Home: NextPage = ({ projects = [], posts = [] }: InferGetStaticPropsType<t
           <h2 className="text-h3 mb-2 font-bold md:text-h2-md md:text-center md:pr-3">{t('projects.title')}</h2>
           <p className="max-w-2xl w-full text-h5 md:text-center md:text-h5-md">{t('projects.sub-heading')}</p>
         </div>
-        <div>{projects?.map(({ id, ...project }: Project, index: number) => <Project key={id} {...project} inverted={!!(index % 2)} />)}</div>
+        <div>
+          {projects?.map(({ id, ...project }: Project, index: number) => (
+            <Project key={id} {...project} inverted={!!(index % 2)} />
+          ))}
+        </div>
       </section>
       <section id="blog" className="mb-16 px-3">
         <div className="md:flex items-center justify-between max-w-5xl w-full mx-auto mt-28 mb-6 md:mb-28">
           <h2 className="text-h3 mb-2 font-bold md:text-h2-md md:text-center md:pr-3">{t('blog.title')}</h2>
           <p className="max-w-2xl w-full text-h5 md:text-center md:text-h5-md">{t('blog.sub-heading')}</p>
         </div>
-        <div className="flex flex-wrap">{posts?.map(({ id, ...post }: Post, index: number) => <Post key={id} index={index + 1} actionText={t('blog.action')} {...post} />)}</div>
+        <div className="flex flex-wrap">
+          {posts?.map(({ id, ...post }: Post, index: number) => (
+            <Post key={id} index={index + 1} actionText={t('blog.action')} {...post} />
+          ))}
+        </div>
       </section>
     </>
   )
